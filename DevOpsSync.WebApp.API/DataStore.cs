745c6f7bc864ab8eeca6be00e2ea96a93835ed52
sidkcr <sidkcr@gmail.com>
2020-01-28 04:50:55 +0530

    Add temporary storage

---
+++ DevOpsSync.WebApp.API/DataStore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DevOpsSync.WebApp.API
+{
+    public interface IDataStore
+    {
+        Dictionary<string, string> Storage { get; set; }
+    }
+
+    public class DataStore : IDataStore
+    {
+        public DataStore()
+        {
+            Storage = new Dictionary<string, string>();
+        }
+
+        public Dictionary<string, string> Storage { get; set; }
+    }
+}
