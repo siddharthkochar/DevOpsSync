aa4ce04ea84027b5d3ae4bff8eeb8d08462ad2c6
sidkcr <sidkcr@gmail.com>
2020-01-27 13:43:10 +0530

    Add github authorization

---
+++ DevOpsSync.WebApp.API/Code/GitHub.cs
@@ -0,0 +1,30 @@
+﻿using DevOpsSync.WebApp.API.Models.GitHub;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace DevOpsSync.WebApp.API.Code
+{
+    public interface IGitHub
+    {
+        Task<Token.Response> GetTokenAsync(Token.Request parameters);
+    }
+
+    public class GitHub : IGitHub
+    {
+        public async Task<Token.Response> GetTokenAsync(Token.Request parameters)
+        {
+            var client = new RestClient("https://github.com/login");
+            client.Timeout = -1;
+            var request = new RestRequest("/oauth/access_token", Method.POST);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("client_id", parameters.ClientId);
+            request.AddParameter("client_secret", parameters.CilentSecret);
+            request.AddParameter("code", parameters.Code);
+            IRestResponse response = await client.ExecuteAsync(request);
+            var content = JsonConvert.DeserializeObject<Token.Response>(response.Content);
+            return content;
+        }
+    }
+}
