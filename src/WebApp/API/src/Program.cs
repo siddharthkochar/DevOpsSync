93fc6c996042cd1717ab2106d893454a819684d2
Siddharth Kochar <sidkcr@gmail.com>
2020-01-30 00:41:05 +0530

    Organize project structure (#5)

--- src/WebApp/API/src/Program.cs
+++ src/WebApp/API/src/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace DevOpsSync.WebApp.API
 {
@@ -13,7 +9,10 @@ namespace DevOpsSync.WebApp.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .ConfigureHostConfiguration(config => config.SetBasePath(Directory.GetCurrentDirectory()))
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
