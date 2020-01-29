f8543c4afa6ce5ad5f55d3bafd20920b135253c7
Siddharth Kochar <sidkcr@gmail.com>
2020-01-30 00:25:42 +0530

    Feature/GitHub webhooks (#4)
    * Add APIs to create and handle webhooks
    
    * Add missing namespace

--- DevOpsSync.WebApp.API/Program.cs
+++ DevOpsSync.WebApp.API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace DevOpsSync.WebApp.API
 {
@@ -7,7 +9,10 @@ namespace DevOpsSync.WebApp.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .ConfigureHostConfiguration(config => config.SetBasePath(Directory.GetCurrentDirectory()))
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
