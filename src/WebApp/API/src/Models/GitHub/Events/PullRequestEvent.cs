5558cacd0b1bff8a61e10226b14cd8a8e0e90a43
Siddharth Kochar <sidkcr@gmail.com>
2020-01-31 06:31:40 +0530

    Extract webhook event information (#12)
    * extract webhook event information
    
    * Remove unwanted class

---
+++ src/WebApp/API/src/Models/GitHub/Events/PullRequestEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DevOpsSync.WebApp.API.Models.GitHub.Events
+{
+    public class PullRequestEvent
+    {
+        public string action { get; set; }
+        public Pull_Request pull_request { get; set; }
+    }
+
+    public class Pull_Request
+    {
+        public string body { get; set; }
+    }
+}
