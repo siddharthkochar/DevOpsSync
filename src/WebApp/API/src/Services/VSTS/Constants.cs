b05e1c9ee60c2783eab80a1e0dbba8f65d522a6e
Nakul Bhat <nbsy86@gmail.com>
2020-02-02 11:32:47 +0530

    1. Added new vsts controller. 2. Added private method in github controller to set item status. 3. Changed redirect url in appsettings.

--- src/WebApp/API/src/Services/VSTS/Constants.cs
+++ src/WebApp/API/src/Services/VSTS/Constants.cs
@@ -2,6 +2,7 @@ namespace DevOpsSync.WebApp.API.Services.VSTS
 {
     public static class Constants
     {
+        public static string VSTSServiceKey = nameof(VSTSServiceKey);
         public static VSTSOp VSTSOpEnum = VSTSOp.GetInstance();
     }
 }
